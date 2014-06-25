ac0805378437a8c288e9112396484a6721e9867e
songchenglin <songchenglin92@gmail.com>
2014-06-25 10:31:54 +0800

    Add blog app to project

--- myblog/settings.py
+++ myblog/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
+    'home',#home page
+    'blog',#blog app
 )
 
 MIDDLEWARE_CLASSES = (
